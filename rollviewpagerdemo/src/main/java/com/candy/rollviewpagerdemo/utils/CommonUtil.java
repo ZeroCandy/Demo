c99c821753b6694ad9f935d80124cbfab3351c84
ZeroCandy <ct-zsylyt@163.com>
2015-12-20 22:10:03 +0800

    未完成指示点切换版本

---
+++ rollviewpagerdemo/src/main/java/com/candy/rollviewpagerdemo/utils/CommonUtil.java
@@ -0,0 +1,7 @@
+package com.candy.rollviewpagerdemo.utils;
+
+/**
+ * Created by 帅阳 on 2015/12/13.
+ */
+public class CommonUtil {
+}
---
+++ rollviewpagerdemo/src/main/java/com/candy/rollviewpagerdemo/widget/RollViewPager.java
@@ -0,0 +1,7 @@
+package com.candy.rollviewpagerdemo.widget;
+
+/**
+ * Created by 帅阳 on 2015/12/9.
+ */
+public class RollViewPager {
+}
